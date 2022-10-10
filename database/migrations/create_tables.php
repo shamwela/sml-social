b0af352fbe259d86ba1054cfa18a7811240b0455
Sha Mwe La <shamwela2002@gmail.com>
2022-10-10 17:27:14 +0630

    Add migrations

---
+++ database/migrations/create_tables.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('email', 50)->unique();
+            $table->string('password', 100);
+            $table->timestamps();
+        });
+
+        Schema::create('friends_users', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('friend_id')->constrained('users')->onDelete('cascade');
+
+            $table->primary(['user_id', 'friend_id']);
+        });
+
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('text', 500);
+            $table->string('image_name', 100)->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends_users');
+        Schema::dropIfExists('posts');
+        Schema::dropIfExists('users');
+    }
+};
