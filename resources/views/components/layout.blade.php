911256f2b3bda3f96813dc53f7ab33032dbe1ae5
Sha Mwe La <shamwela2002@gmail.com>
2022-10-11 16:44:34 +0630

    Improve styles

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,14 +4,14 @@
 	    <title>{{ $title }}</title>
       @vite('resources/css/app.css')
     </head>
-    <body>
+    <body class='bg-gray-300 flex flex-col gap-y-4'>
       {{-- If logged in --}}
       @if (Cookie::get('email') and Cookie::get('password'))
-      <nav class='flex gap-x-8'>
+      <nav class='flex gap-x-8 justify-center'>
         <a href='{{ route("home") }}'>Home</a>
         <a href='{{ route("user.show", Cookie::get('user_id')) }}'>View profile</a>
       </nav>
       @endif
-        {{ $slot }}
+        <main class='mx-auto w-full md:max-w-md flex flex-col gap-y-4'>{{ $slot }}</main>
     </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,22 @@
 <x-layout>
-  <x-slot name='title'>Home</x-slot>
-  <h1>Home</h1>
+  <x-slot name='title'>SML Social</x-slot>
+  <h1>SML Social</h1>
   
   <a href='{{ route("post.create") }}'>Create a new post</a>   
   
   @foreach ($posts as $post)
     <div>
+      <div class='flex gap-x-4'>
+        <a href='{{ route("post.edit", $post->id) }}'>Edit</a>
+        
+        <form action='{{ route("post.destroy", $post->id) }}' method='post'>
+          @method('delete')
+          @csrf
+          <button onclick='return confirm("Are you sure?")' type='submit'>Delete</button>
+        <form>
+      </div>
+
       <a href='{{ route("post.show", $post->id) }}'>{{ $post->text }}</a>
-      <a href='{{ route("post.edit", $post->id) }}'>Edit</a>
-      
-      <form action='{{ route("post.destroy", $post->id) }}' method='post'>
-        @method('delete')
-        @csrf
-        <button onclick='return confirm("Are you sure?")' type='submit'>Delete</button>
-      <form>
     </div>
   @endforeach
 </x-layout>
