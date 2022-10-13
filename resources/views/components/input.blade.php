8024abd243c055cd4294d213a0b17a52c91c8144
Sha Mwe La <shamwela2002@gmail.com>
2022-10-13 17:18:02 +0630

    Improve

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,13 +1,33 @@
 <div class='flex flex-col gap-y-1'>
   <label for={{ $name }}>
     {{ ucfirst($name) }}
+    @if (isset($optional))
+      (optional)
+    @endif
   </label>
   
   @if ($type === 'textarea')
-    <textarea name={{ $name }} id={{ $name }} required>{{ old($name) }}</textarea>
+    <textarea
+      name={{ $name }}
+      id={{ $name }}
+      
+      @if (!isset($optional))
+        required
+      @endif
+    >
+      {{ old($name) }}
+    </textarea>
   @else
-    <input name={{ $name }} id={{ $name }} value='{{ old($name) }}' 
-      type={{ $type }} {{ isset($optional) ? '' : 'required' }}>
+    <input
+      name={{ $name }}
+      id={{ $name }}
+      value='{{ old($name) }}' 
+      type={{ $type }}
+
+      @if (!isset($optional))
+        required
+      @endif
+    >
   @endif
 
   @error($name)
