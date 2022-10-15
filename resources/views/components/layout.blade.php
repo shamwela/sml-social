fda729ace313ac9d93e42ea58fedd10bba409e56
Sha Mwe La <shamwela2002@gmail.com>
2022-10-15 18:34:36 +0630

    Improve

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,7 @@
         <x-navigation />
       @endif
 
-      <main class='mx-auto w-full md:max-w-md flex flex-col gap-y-4 min-h-screen p-4'>
+      <main class='mx-auto w-full md:max-w-md flex flex-col gap-y-4 p-4'>
         <h1>{{ $title }}</h1>
         {{ $slot }}
       </main>
